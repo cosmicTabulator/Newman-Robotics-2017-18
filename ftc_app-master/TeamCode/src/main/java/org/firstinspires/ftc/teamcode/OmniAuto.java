888bb90005b28ad3c801031bd4280f7e2f0198a0
gmc1221 <grahamcooke1221@gmail.com>
2016-11-08 11:55:40 -0500

    Autonomous Fixes and Cleanup
    Fixed several potentialerrors in the autonomous program, specifically
    the drive by encoders method. Also added comments and cleaned up code
    somewhat

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniAuto.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniAuto.java
@@ -32,7 +32,7 @@ public class OmniAuto extends LinearOpMode{
 
             gyro.getHeading();
 
-            if(touch.isPressed() == true){
+            if(touch.isPressed()){
 
             }
 
@@ -43,7 +43,11 @@ public class OmniAuto extends LinearOpMode{
 
     }
 
-    void moveD(double theta, double r, long d) throws InterruptedException{
+    void moveD(double angle, double speed, long distance) throws InterruptedException{
+
+        //find our position before moving
+        long startL = robot.FRight.getCurrentPosition();
+        long startR = robot.FLeft.getCurrentPosition();
 
         double fright;
         double bright;
@@ -53,52 +57,63 @@ public class OmniAuto extends LinearOpMode{
         double pos = 0;
         long rpos;
         long lpos;
-        double x1 = 0;
-        double x2 = 0;
-        double y1 = 0;
-        double y2 = 0;
-        double xf = 0;
-        double yf = 0;
+        double x1;
+        double x2;
+        double y1;
+        double y2;
+        double xf;
+        double yf;
 
-        double y = Math.sin(theta);
-        double x = Math.cos(theta);
-        x = r*x;
-        y = r*y;
+        //calculate the and and y powers from the desired angle of motion
+        double y = Math.sin(angle);
+        double x = Math.cos(angle);
+        x = speed*x;
+        y = speed*y;
 
 
+        //calculate the motor powers
         fright = -x + y;
         bright = x + y;
         fleft = -x - y;
         bleft = x - y;
 
+        //print the motor powers to the phones
         telemetry.addData("Front Right", fright);
         telemetry.addData("Back Right", bright);
         telemetry.addData("Front Left", fleft);
         telemetry.addData("Back Left", bleft);
         telemetry.update();
 
+        //power the motors
         robot.FRight.setPower(fright);
         robot.BRight.setPower(bright);
         robot.FLeft.setPower(fleft);
         robot.BLeft.setPower(bleft);
 
-        while(d < pos){
 
-            rpos = robot.FRight.getCurrentPosition();
-            lpos = robot.FLeft.getCurrentPosition();
+        //while the distance travelled has not surpassed the target
+        while(distance < pos){
 
-            x1 = rpos/(1/Math.sqrt(2));
-            y1 = rpos/(1/Math.sqrt(2));
-            x2 = lpos/(1/Math.sqrt(2));
-            y2 = lpos/(1/Math.sqrt(2));
+            //get encoder values(by subtracting the values before we started moving)
+            rpos = robot.FRight.getCurrentPosition() - startR;
+            lpos = robot.FLeft.getCurrentPosition() - startL;
 
+            //get the x and y parts of each motor vector
+            x1 = -rpos/Math.sqrt(2);
+            y1 = rpos/Math.sqrt(2);
+            x2 = -lpos/Math.sqrt(2);
+            y2 = -lpos/Math.sqrt(2);
+
+            //add the vectors
             xf = x1 + x2;
             yf = y1 + y2;
 
+            //calculate the magnitude of the final vector
             pos = Math.sqrt(Math.pow(xf, 2) + Math.pow(yf, 2));
 
         }
 
+        //stop the motors
         robot.FRight.setPower(0);
         robot.BRight.setPower(0);
         robot.FLeft.setPower(0);
@@ -112,30 +127,35 @@ public class OmniAuto extends LinearOpMode{
         double fleft;
         double bleft;
 
+        //calculate the x and y powers based on the angle of motion
         double y = Math.sin(theta);
         double x = Math.cos(theta);
         x = r*x;
         y = r*y;
 
-
+        //calculate the motor powers
         fright = -x + y;
         bright = x + y;
         fleft = -x - y;
         bleft = x - y;
 
+        //print the motor powers to the phone
         telemetry.addData("Front Right", fright);
         telemetry.addData("Back Right", bright);
         telemetry.addData("Front Left", fleft);
         telemetry.addData("Back Left", bleft);
         telemetry.update();
 
+        //power the motors
         robot.FRight.setPower(fright);
         robot.BRight.setPower(bright);
         robot.FLeft.setPower(fleft);
         robot.BLeft.setPower(bleft);
 
+        //wait for the desired time
         Thread.sleep(t);
 
+        //stop the motors
         robot.FRight.setPower(0);
         robot.BRight.setPower(0);
         robot.FLeft.setPower(0);
