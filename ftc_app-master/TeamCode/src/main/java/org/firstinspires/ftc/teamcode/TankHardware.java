eb361ccd4c518eb267250c65fcf41640577d2c9f
gmc1221 <grahamcooke1221@gmail.com>
2016-12-08 21:06:39 -0500

    Added Preliminary Tank Drive Code
    Wrote in a bit of code for the potential tank drive, no tests yet though

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankHardware.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by Graham Cooke on 9/28/2016.
+ */
+public class TankHardware {
+
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    public DcMotor right = null;
+    public DcMotor left = null;
+    public DcMotor arm = null;
+    public DcMotor sweeper = null;
+
+    public TankHardware(){
+    }
+
+    public void init(HardwareMap ahwMap){
+
+        hwMap = ahwMap;
+
+        right = hwMap.dcMotor.get("right motor");
+        left = hwMap.dcMotor.get("left motor");
+
+        arm = hwMap.dcMotor.get("arm motor");
+        sweeper = hwMap.dcMotor.get("sweeper motor");
+
+        right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        sweeper.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        right.setPower(0);
+        left.setPower(0);
+        arm.setPower(0);
+        sweeper.setPower(0);
+
+    }
+
+
+    public void waitForTick(long periodMs) throws InterruptedException{
+
+        long remaining = periodMs - (long) period.milliseconds();
+
+        if (remaining > 0){
+            Thread.sleep(remaining);
+
+            period.reset();
+        }
+
+    }
+}
+
+// stuff
---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankOpMode.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by Graham Cooke on 9/28/2016.
+ */
+@TeleOp(name = "TankOp", group = "Tank")
+
+public class TankOpMode extends LinearOpMode {
+
+    TestHardware robot = new TankHardware();
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+        waitForStart();
+
+        double r = 0;
+        double l = 0;
+        double s;
+        double a;
+
+        while(opModeIsActive()){
+
+
+            r = -gamepad1.right_stick_y;
+            l = -gamepad1.left_stick_y;
+
+            if(gamepad1.left_bumper){
+              s = 1;
+            }
+            else{
+              s = 0;
+            }
+
+            if(gamepad1.right_bumper){
+                a = -0.6;
+            }
+            else if(gamepad1.right_trigger > 0.1){
+                a = -0.8;
+            }
+            else{
+                a = 0;
+            }
+
+            telemetry.addData("Right", r);
+            telemetry.addData("Left", l);
+            telemetry.addData("Sweeper", s);
+            telemetry.addData("Arm", a);
+            telemetry.update();
+
+            robot.right.setPower(r);
+            robot.left.setPower(l);
+            robot.sweeper.setPower(s);
+            robot.arm.setPower(a);
+
+            robot.waitForTick(10);
+            idle();
+        }
+
+    }
+}
