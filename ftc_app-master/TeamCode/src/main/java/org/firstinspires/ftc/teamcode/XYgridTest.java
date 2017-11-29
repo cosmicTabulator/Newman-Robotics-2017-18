a2344c7e26747691afeb7e96328d321e315152ec
brendanmatulis <31901259+brendanmatulis@users.noreply.github.com>
2017-11-29 15:44:42 -0500

    Changes to Autonmous Basics
    Some additions:
    - factor to convert inches to cm easily
    - encoder checks to see how far robot has gone and then rotates accordingly depending on which starting place
    - (incomplete)

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XYgridTest.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XYgridTest.java
@@ -27,26 +27,113 @@ public class XYgridTest extends LinearOpMode {
         static float rightMotor, leftMotor;
 
         //Wheel diameter, used to find circumference in CM
+        final static double inchToCM = 2.54; //Conversion factor of 1 inch to 2.54 CM
+
         static double diameter = 10.16;
-        static double circumference = Math.PI * diameter;
-        static double rotations = 0;
-        static double totalDistance = circumference * rotations;
+        static double wheelCircumference = Math.PI * diameter;
+        static double encoderValue;
+        static double numRotations;
+        static double totalDistance = circumference * numRotations;
+
+
+        //inchToCm conversion factor convers inches to CM
+        //Robot width used to calculate rotations needed for a turn
+        static double widthRobot = 40 * inchToCm; //40 inches
+
+
+        // 1/4th  of circumference (C=pi*diameter)
+        //Distance travelled by wheels when turning
+        //Used to calculate number of rotations for turning 90 degrees
+        double rightAngleTurnDistance = (Math.PI * widthRobot) / 4;
+        double rightAngleTurnRotations = rightAngleTurnDistance / wheelCircumference;
+
 
         TankHardwareSensors robot = new TankHardwareSensors();
 
         public static void orientateRobot() {
 
             //Orientates robot with respect to wall
-            /*if (distanceLeftY > distanceRightY) {
+            if (distanceLeftY > distanceRightY) {
                 leftMotor += .05;
             } else if (distanceLeftY < distanceRightY) {
                 rightMotor += .05;
             } else {
                 //Go forwards
-            } */
+            }
 
-        };
+        }
 
+        public static void rotate_90_degrees_clockwise() {
+
+            final double initial_NumRotations = numRotations;
+            while (change_in_numRotations < rightAngleTurnRotations) {
+                encoderValue = robot.right.getCurrentPosition();
+                numRotations = encoderValue / 1440;
+
+                double change_in_numRotations = numRotations - initial_NumRotations;
+
+                leftMotor = 0.5;
+                rightMotor = -0.5;
+            }
+            rightMotor = 0.0;
+            leftMotor = 0.0;
+        }
+
+    public static void rotate_90_degrees_anticlockwise() {
+
+        final double initial_NumRotations = numRotations;
+        while (change_in_numRotations < rightAngleTurnRotations) {
+            encoderValue = robot.right.getCurrentPosition();
+            numRotations = encoderValue / 1440;
+
+            double change_in_numRotations = numRotations - initial_NumRotations;
+
+            leftMotor = -0.5;
+            rightMotor = 0.5;
+        }
+        rightMotor = 0.0;
+        leftMotor = 0.0;
+    }
+
+    public static void top_left_red_check1() {
+        //If the robot travels 36" (94.44 cm) or more, robot stops
+        //Goal is 36" from top left red start
+        //This is the same for top right blue start
+        //For the bottom red/blue starts, set distance to 60.98 CM (ie 24 in)...
+        //Then, rotate robot 90 degrees and move forward 30.48 CM (ie 12 in)
+        if (totalDistance >= 36 * inchToCm) {
+            //Stops robot once robot has traveled 36" or more
+            leftMotor = 0;
+            rightMotor = 0;
+        }
+        // turns towards goal
+        rotate_90_degrees_anticlockwise();
+    }
+
+    public static void top_right_blue_check1() {
+        if (totalDistance >= 36 * inchToCm) {
+            //Stops robot once robot has traveled 36" or more
+            leftMotor = 0;
+            rightMotor = 0;
+        }
+
+        //turns towards goal
+        rotate_90_degrees_clockwise();
+    }
+
+    public static void bottom_left_red_check1() {
+        if (totalDistance >= 24 * inchToCM) {
+            leftMotor = 0;
+            rightMotor = 0;
+
+            rotate_90_degrees_clockwise();
+        }
+    }
+
+    public static void bottom_left_red_check2() {
+            //should check to see if it moves another 12 inches and then turn towards goal
+            //note: should find change in rotations for the 12 inches
+    }
 
         @Override
         public void runOpMode() throws InterruptedException {
@@ -63,20 +150,22 @@ public class XYgridTest extends LinearOpMode {
                 robot.left.setPower(leftMotor);
                 robot.right.setPower(rightMotor);
 
-                rotations = robot.right.getCurrentPosition() / 1440;
+                encoderValue = robot.right.getCurrentPosition();
+                numRotations =  encoderValue / 1440;
 
-                //If the robot travels 36" (94.44 cm) or more, robot stops
-                //Goal is 36" from top left red start
-                //This is the same for top right blue start
-                //For the bottom red/blue starts, set distance to 60.98 CM (ie 24 in)...
-                //Then, rotate robot 90 degrees and move forward 30.48 CM (ie 12 in)
-                if (totalDistance >= 94.44) {
-                    leftMotor = 0;
-                    rightMotor = 0;
-                }
+
+                //runs program for top left red
+                //top_left_red_check1();
+
+                //runs program for top right blue
+                //sees how far it has gone and then rotates or stops accordingly
+                //top_right_blue_check1();
+
 
                 robot.waitForTick(10);
                 idle();
+
+
             }
         }
 
